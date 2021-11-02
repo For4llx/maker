3e2be4b9c1d71376260a15dfc25b09d31b5246b0
Nobime <romeo.nobime@mailo.com>
2021-11-02 12:15:32 +0100

    first commit develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+import "./css/style.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
