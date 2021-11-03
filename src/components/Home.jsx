563f046e735ccc3f88974982925e097a1cfcf955
Nobime <romeo.nobime@mailo.com>
2021-11-03 16:21:58 +0100

    layout-2 tablet terminé

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,38 +20,46 @@ function Home() {
         <ul className="layout-2__ul">
           <li className="layout-2__list">
             <article className="layout-2__perk layout-2__perk--passions">
-              <h3 className="layout-2__title">Indulge your passions</h3>
-              <p className="layout-2__body">
-                Your passions shouldn't be just for the weekend. Earn a living
-                doing what you love.
-              </p>
+              <div className="layout-2__container">
+                <h3 className="layout-2__title">Indulge your passions</h3>
+                <p className="layout-2__body">
+                  Your passions shouldn't be just for the weekend. Earn a living
+                  doing what you love.
+                </p>
+              </div>
             </article>
           </li>
           <li className="layout-2__list">
             <article className="layout-2__perk layout-2__perk--financial">
-              <h3 className="layout-2__title">Gain financial freedom</h3>
-              <p className="layout-2__body">
-                Start making money work for you. There’s nothing quite like
-                earning while you sleep.
-              </p>
+              <div className="layout-2__container">
+                <h3 className="layout-2__title">Gain financial freedom</h3>
+                <p className="layout-2__body">
+                  Start making money work for you. There’s nothing quite like
+                  earning while you sleep.
+                </p>
+              </div>
             </article>
           </li>
           <li className="layout-2__list">
             <article className="layout-2__perk layout-2__perk--lifestyle">
-              <h3 className="layout-2__title">Choose your lifestyle</h3>
-              <p className="layout-2__body">
-                Own your daily schedule. Fancy a lie-in? Go for it! Take charge
-                of your week.
-              </p>
+              <div className="layout-2__container">
+                <h3 className="layout-2__title">Choose your lifestyle</h3>
+                <p className="layout-2__body">
+                  Own your daily schedule. Fancy a lie-in? Go for it! Take
+                  charge of your week.
+                </p>
+              </div>
             </article>
           </li>
           <li className="layout-2__list">
             <article className="layout-2__perk layout-2__perk--remote">
-              <h3 className="layout-2__title">Work from anywhere</h3>
-              <p className="layout-2__body">
-                Selling online means not being pinned down. Want to work AND
-                travel? Go for it!
-              </p>
+              <div className="layout-2__container">
+                <h3 className="layout-2__title">Work from anywhere</h3>
+                <p className="layout-2__body">
+                  Selling online means not being pinned down. Want to work AND
+                  travel? Go for it!
+                </p>
+              </div>
             </article>
           </li>
         </ul>
