c67cdbc81272718f568946c63d94ba8d948f5559
Nobime <romeo.nobime@mailo.com>
2021-11-04 19:28:55 +0100

    layout-1 mobile terminé

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             Get paid for the work you{" "}
             <span className="paragraph__emphase">love</span> to do.
           </h2>
-          <p className="paragraph__body">
+          <p className="layout-1__body paragraph__body">
             The 9-5 grind is so last century. We believe in living life on your
             own terms. Whether you’re looking to escape the rat race or set up a
             side hustle, we’ve got you covered.
