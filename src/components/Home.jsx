758f46caa13c9869201af161d098a5aace9740f5
Nobime <romeo.nobime@mailo.com>
2021-11-04 14:23:15 +0100

    HTML validé

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,111 @@
 import React from "react";
 
 function Home() {
-  return <main className="home"></main>;
+  return (
+    <main>
+      <div>
+        <article>
+          <h2>
+            Get paid for the work you <span>love</span> to do.
+          </h2>
+          <p>
+            The 9-5 grind is so last century. We believe in living life on your
+            own terms. Whether you’re looking to escape the rat race or set up a
+            side hustle, we’ve got you covered.
+          </p>
+        </article>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <article>
+              <h3>Indulge your passions</h3>
+              <p>
+                Your passions shouldn't be just for the weekend. Earn a living
+                doing what you love.
+              </p>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>Gain financial freedom</h3>
+              <p>
+                Start making money work for you. There’s nothing quite like
+                earning while you sleep.
+              </p>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>Choose your lifestyle</h3>
+              <p>
+                Own your daily schedule. Fancy a lie-in? Go for it! Take charge
+                of your week.
+              </p>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>Work from anywhere</h3>
+              <p>
+                Selling online means not being pinned down. Want to work AND
+                travel? Go for it!
+              </p>
+            </article>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <article>
+          <h2>Our pricing plans</h2>
+          <p>
+            We only make money when our creators make money. Our plans are
+            always affordable, and it’s completely free to get started.
+          </p>
+        </article>
+        <div>
+          <div>
+            <article>
+              <h3>Dip your toe</h3>
+              <p>
+                Just getting started? No problem at all! Our free plan will take
+                you a long way.
+              </p>
+            </article>
+            <article>
+              <h3>Free</h3>
+              <ul>
+                <li>Unlimited products</li>
+                <li>Basic analytics</li>
+                <li>Limited marketplace exposure</li>
+                <li>10% fee per transaction</li>
+              </ul>
+            </article>
+          </div>
+          <div>
+            <article>
+              <h3>Dive right in</h3>
+              <p>
+                Ready for the big time? Our paid plan will help you take your
+                business to the next level.
+              </p>
+            </article>
+            <article>
+              <h3>
+                <span>$25.00</span> /month
+              </h3>
+              <ul>
+                <li>Custom domain</li>
+                <li>Advanced analytics and reports</li>
+                <li>High marketplace visibility</li>
+                <li>5% fee per transaction</li>
+              </ul>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
